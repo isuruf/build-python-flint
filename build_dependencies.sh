7e5b75027e3ee18456e7a3eb5629c52f4bbe6a9c
xoviat <xoviat@users.noreply.github.com>
2017-10-09 18:05:31 -0500

    build_dependencies: enable debug

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -12,7 +12,7 @@ wget http://mpir.org/mpir-2.7.0.tar.bz2
 tar -xf mpir-2.7.0.tar.bz2
 cd mpir-2.7.0
 ./configure --enable-gmpcompat --prefix=$HOME/deps --disable-static $PLATFORM_ARGS
-make -j4 > /dev/null 2>&1
+make -j4
 make install
 cd ..
 
@@ -20,7 +20,7 @@ wget http://www.mpfr.org/mpfr-3.1.4/mpfr-3.1.4.tar.bz2
 tar -xf mpfr-3.1.4.tar.bz2
 cd mpfr-3.1.4
 ./configure --with-gmp=$HOME/deps --prefix=$HOME/deps --disable-static $PLATFORM_ARGS
-make -j4 > /dev/null 2>&1
+make -j4
 make install
 cd ..
 
@@ -28,7 +28,7 @@ wget http://flintlib.org/flint-2.5.2.tar.gz
 tar -xf flint-2.5.2.tar.gz
 cd flint-2.5.2
 ./configure --with-gmp=$HOME/deps --with-mpfr=$HOME/deps --prefix=$HOME/deps --disable-static
-make -j4 > /dev/null 2>&1
+make -j4
 make install
 cd ..
 
